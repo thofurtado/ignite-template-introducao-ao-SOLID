a153239a5a4fdfa13d086825b4bf67ace3bd2acc
Thomás Furtado <32690619+thofurtado@users.noreply.github.com>
2021-03-22 13:24:15 -0300

    finalizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,13 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
+    const user_id = request.headers.user_id as string;
 
     try {
       const users = this.listAllUsersUseCase.execute({ user_id });
       return response.status(201).send(users);
     } catch (err) {
-      return response.status(404).send({ error: err.message });
+      return response.status(400).send({ error: err.message });
     }
   }
 }
